2eec78080db306e0333b317a57f021c9aa28e31d
Aditya <Adityapurviya67@gmail.com>
2025-12-11 12:46:22 +0530

    Add files via upload

---
+++ get_vendor_summary.py
@@ -0,0 +1,123 @@
+import sqlite3
+import pandas as pd
+import logging
+from ingestion_db import ingest_db
+
+logging.basicConfig(
+    filename="logs/get_vendor_summary.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    filemode="a"
+)
+
+def create_vendor_summary(conn):
+    """ This function will merge the different tables to get the overall vendor summary and adds new columns in the resultant data. """
+    vendor_sales_summary = pd.read_sql_query ( """ WITH FreightSummary AS (
+        SELECT
+            VendorNumber,
+            SUM(Freight) AS FreightCost
+        FROM vendor_invoice
+        GROUP BY VendorNumber
+    ),
+
+    PurchaseSummary AS (
+        SELECT
+            p.VendorNumber,
+            p.VendorName,
+            p.Brand,
+            p.Description,
+            p.PurchasePrice,
+            pp.Price AS ActualPrice,
+            pp.Volume,
+            SUM(p.Quantity) AS TotalPurchaseQuantity,
+            SUM(p.Dollars) AS TotalPurchaseDollars
+        FROM purchases p
+        JOIN purchase_prices pp
+            ON p.Brand = pp.Brand
+        WHERE p.PurchasePrice > 0
+        GROUP BY p.VendorNumber, p.VendorName, p.Brand, p.Description, p.PurchasePrice, pp.Price, pp.Volume
+    ),
+
+    SalesSummary AS (
+        SELECT
+            VendorNo,
+            Brand,
+            SUM(SalesQuantity) AS TotalSalesQuantity,
+            SUM(SalesDollars) AS TotalSalesDollars,
+            SUM(SalesPrice) AS TotalSalesPrice,
+            SUM(ExciseTax) AS TotalExciseTax
+        FROM sales
+        GROUP BY VendorNo, Brand
+    )
+
+    SELECT
+        ps.VendorNumber,
+        ps.VendorName,
+        ps.Brand,
+        ps.Description,
+        ps.PurchasePrice,
+        ps.ActualPrice,
+        ps.Volume,
+        ps.TotalPurchaseQuantity,
+        ps.TotalPurchaseDollars,
+        ss.TotalSalesQuantity,
+        ss.TotalSalesDollars,
+        ss.TotalSalesPrice,
+        ss.TotalExciseTax,
+        fs.FreightCost
+    FROM PurchaseSummary ps
+    LEFT JOIN SalesSummary ss
+        ON ps.VendorNumber = ss.VendorNo
+        AND ps.Brand = ss.Brand
+    LEFT JOIN FreightSummary fs
+        ON ps.VendorNumber = fs.VendorNumber
+    ORDER BY ps.TotalPurchaseDollars DESC""", conn)
+
+    return vendor_sales_summary
+
+def clean_data(df):
+    """This function will clean the data."""
+    
+    # Changing datatype to float
+    df['Volume'] = df['Volume'].astype('float')
+    
+    # Filling missing values with 0
+    df.fillna(0, inplace=True)
+    
+    # Removing spaces from categorical columns
+    df['VendorName'] = df['VendorName'].str.strip()
+    df['Description'] = df['Description'].str.strip()
+
+    # Creating new columns for better analysis
+    df['GrossProfit'] = df['TotalSalesDollars'] - df['TotalPurchaseDollars']
+    df['ProfitMargin'] = (df['GrossProfit'] / df['TotalSalesDollars']) * 100
+    df['StockTurnover'] = df['TotalSalesQuantity'] / df['TotalPurchaseQuantity']
+    df['SalesToPurchaseRatio'] = df['TotalSalesDollars'] / df['TotalPurchaseDollars']
+
+    return df
+
+
+    if __name__ == '__main__':
+    # Creating database connection
+    conn = sqlite3.connect('inventory.db')
+    logging.info('Creating Vendor Summary Table.....')
+
+    # Creating vendor summary
+    summary_df = create_vendor_summary(conn)
+
+    # Logging the first few rows of the summary
+    logging.info(summary_df.head())
+
+    # Cleaning the data
+    logging.info('Cleaning Data.....')
+    clean_df = clean_data(summary_df)
+
+    # Logging the first few rows of cleaned data
+    logging.info(clean_df.head())
+
+    # Ingesting the cleaned data into the database
+    logging.info('Ingesting data.....')
+    ingest_db(clean_df, 'vendor_sales_summary', conn)
+
+    # Logging the completion of the process
+    logging.info('Completed')
